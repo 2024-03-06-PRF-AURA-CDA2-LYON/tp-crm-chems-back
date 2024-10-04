8e763ae856d08217d363cdf3a9d59e3fb5390f38
chems meziou <chemsmeziou@chemss-MacBook-Air.local>
2024-10-04 16:20:56 +0300

    ajout de l'annotation @CrossOrigin(origins = http://localhost:4200)  sur les controller

--- tp-evaluation/src/main/java/crm/chems/tp_evaluation/controller/CustomerController.java
+++ tp-evaluation/src/main/java/crm/chems/tp_evaluation/controller/CustomerController.java
@@ -9,12 +9,15 @@ import org.springframework.web.server.ResponseStatusException;
 import crm.chems.tp_evaluation.entity.Customer;
 import crm.chems.tp_evaluation.repository.CustomerRepository;
 import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
--- tp-evaluation/src/main/java/crm/chems/tp_evaluation/controller/OrdersController.java
+++ tp-evaluation/src/main/java/crm/chems/tp_evaluation/controller/OrdersController.java
@@ -1,6 +1,5 @@
 package crm.chems.tp_evaluation.controller;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,11 +9,14 @@ import org.springframework.web.server.ResponseStatusException;
 import crm.chems.tp_evaluation.entity.Orders;
 import crm.chems.tp_evaluation.repository.OrdersRepository;
 import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/orders")
 public class OrdersController {
